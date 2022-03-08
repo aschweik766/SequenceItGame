f309369bcac4a7f6a1715845850a78b432d099f4
aschweik766 <97924485+aschweik766@users.noreply.github.com>
2022-03-08 11:18:24 -0700

    Add files via upload
    initial file upload sandbox.js

---
+++ project1/sandbox.js
@@ -0,0 +1,25 @@
+//whiteboarding//
+//sequence game//
+
+//https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API//
+//drag-drop functionality 
+    //drag-events, handlers, function--fires when:
+        // drag, ondrag handler --a dragged item (element) is dragged
+        //dragend, ondragend handler  --a drag operations (such as release mouse button)
+        //dragenter, ondragenter handler  --a dragged item enters a valid drop target
+        //dragleave, ondragleave handler  --a dragged item leaves a valid drop target
+        //dragover, ondragover handler   --a dragged item is being dragged over valid drop target, every few hundred milliseconds
+        //dragstart, ondragstarg handler--user starts draggin an item
+        //drop, ondrop handler  --an item is dropped on a valid drop target
+
+//create object Image-cards of data(images for transfer)
+
+//create object of number-cards of data (numbers for tranfer)     
+
+//function for drag events
+
+//function for testing card matches
+
+//function for calculating score
+
+//function for indicating game-win/ending style, play-again button, win-alert window
